17c96dff1fbd6bafa6f4e23b1d6bc3978fd5032c
Fernando Mendes <fernando.msantos1@gmail.com>
2020-07-10 10:41:27 -0300

    Find user by id

--- src/main/java/com/mendes/workshopmongo/services/UserService.java
+++ src/main/java/com/mendes/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.mendes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mendes.workshopmongo.domain.User;
 import com.mendes.workshopmongo.repository.UserRepository;
+import com.mendes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
 	}
 	
 }
