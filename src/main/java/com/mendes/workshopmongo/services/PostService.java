51e4eab2c87355e8c7940779b873e0a17f153604
Fernando Mendes <fernando.msantos1@gmail.com>
2020-07-14 10:34:26 -0300

    Post by ID

---
+++ src/main/java/com/mendes/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.mendes.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mendes.workshopmongo.domain.Post;
+import com.mendes.workshopmongo.repository.PostRepository;
+import com.mendes.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}
+	
+}
