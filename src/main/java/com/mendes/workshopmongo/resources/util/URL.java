8f7b47d020141ed06548fcf3c058f9d55bae9552
Fernando Mendes <fernando.msantos1@gmail.com>
2020-07-14 11:20:03 -0300

    Query methods in posts

---
+++ src/main/java/com/mendes/workshopmongo/resources/util/URL.java
@@ -0,0 +1,14 @@
+package com.mendes.workshopmongo.resources.util;
+
+import java.net.URLDecoder;
+
+public class URL {
+
+	public static String decodeParam(String text) {
+		try {
+			return URLDecoder.decode(text, "UTF-8");
+		} catch (Exception e) {
+			return "";
+		}
+	}
+}
